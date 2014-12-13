12df367170e6f73ec531f9b09af01be1d3a25328
Matt Mitchell <geminiimatt@gmail.com>
2014-12-13 08:32:20 -0500

    recent changes. including db

--- amos/ajax.php
+++ amos/ajax.php
@@ -1,12 +1,30 @@
 <?php
 
-	include ("../../inc/config.inc.php");
+	// include ("../../inc/config.inc.php");
+	include 'config.php';
+	include 'opendb.php';
 
-	// CLIENT INFORMATION
-	$fname        = htmlspecialchars(trim($_POST['fname']));
-	$lname        = htmlspecialchars(trim($_POST['lname']));
+	// CUSTOMER INFORMATION
+	$emailAddess	 = htmlspecialchars(trim($_POST['emailAddress']));
+	$businessName	 = htmlspecialchars(trim($_POST['businessName']));
+	$firstName       = htmlspecialchars(trim($_POST['firstName']));
+	$lastName        = htmlspecialchars(trim($_POST['lastName']));
+	$phone			 = htmlspecialchars(trim($_POST['lastName']));
+	$address1		 = htmlspecialchars(trim($_POST['address1']));
+	$address2		 = htmlspecialchars(trim($_POST['address2']));
+	$city		 = htmlspecialchars(trim($_POST['city']));
+	$state		 = htmlspecialchars(trim($_POST['state']));
+	$zip		 = htmlspecialchars(trim($_POST['zip']));
+	$description		 = htmlspecialchars(trim($_POST['description']));
+	
+	
+	$addCustomer  = "INSERT INTO customers (emailAddress,businessName,firstName,lastName,phone,address1,address2,city,state,zip,description) VALUES ('$emailAddress','$businessName','$firstName','$lastName','$phone','$address1','$address2','$city','$state','$zip','$description')";
 
-    $addClient  = "INSERT INTO clients (fname,lname) VALUES ('$fname','$lname')";
-    mysql_query($addClient) or die(mysql_error());
+    
+    mysql_query($addCustomer) or die(mysql_error());
 
+
+
+    include 'closedb.php';
+ 
 ?>
